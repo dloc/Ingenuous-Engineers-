73cde6d25caf9020cc00bfa1a94712fcce531f52
Pini <apv1986@gmail.com>
2015-07-30 03:21:26 -0400

    all attractions functional

--- Site Files/attractions.php
+++ Site Files/attractions.php
@@ -1,6 +1,91 @@
 <?php include 'header.php'; ?>
 
 <!-- Include HTML here -->
-<div style="text-align: center; padding-top: 10%; padding-bottom: 10%;"><span>ATTRACTIONS Under Construction...</span></div>
+<div style="max-width: 85%; margin: 0 auto;">
+<div class="row-fluid" style="padding-top: 10%;">
+    <div class="col-sx-6 col-sm-4">
+        <div class="thumbnail">
+            <a href="beaches.php"><img src="Photos/beach.jpg" alt="Jupiter Beach" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Jupiter Beaches</h3>
+            <a href="beaches.php" class="btn btn-primary" role="button">
+               Learn More
+            </a>             
+        </div>
+    </div>
+    <div class="col-sx-6 col-sm-4">
+        <div class="thumbnail">
+            <a href="stadium.php"><img src="Photos/stadium_behind_home_plate.png" alt="Roger Dean Stadium" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Stadium</h3>
+            <a href="stadium.php" class="btn btn-primary" role="button">
+               Learn More
+            </a>             
+        </div>
+    </div>  
+      <div class="col-sx-6 col-sm-4">
+        <div class="thumbnail">
+            <a href="review-parks.php"><img src="Photos/Entrance_sign_JD_state_Park.jpg" alt="State Park" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Jupiter Parks</h3>
+            <a href="review-parks.php" class="btn btn-primary" role="button">
+               Read/Write Reviews
+            </a>             
+        </div>
+    </div>
+</div>
+</div>
+<br>
+<div style="max-width: 85%; margin: 0 auto; display: table; ">
+<div class="row-fluid" style="padding-top: 10%; padding-bottom: 10%;">
+    <div class="col-sx-6 col-sm-3">
+        <div class="thumbnail">
+            <a href="moons.php"><img src="Photos/europa2.jpg" alt="Europa" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Jupiter Moons</h3>
+            <a href="moons.php" class="btn btn-primary" role="button">
+               Learn More
+            </a>             
+        </div>
+    </div>
+    <div class="col-sx-6 col-sm-3">
+        <div class="thumbnail">
+            <a href="night-life.php"><img src="Photos/diva_plavalaguna_thumb.jpg" alt="Diva Plavalaguna" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Night Life</h3>
+            <a href="night-life.php" class="btn btn-primary" role="button">
+               Learn More
+            </a>             
+        </div>
+    </div>
+    <div class="col-sx-6 col-sm-3">
+        <div class="thumbnail">
+            <a href="storm.php"><img src="Photos/storm.jpg" alt="Storm" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Jupiter Storm</h3>
+            <a href="storm.php" class="btn btn-primary" role="button">
+               Learn More
+            </a>             
+        </div>
+    </div>
+    <div class="col-sx-6 col-sm-3">
+        <div class="thumbnail">
+            <a href="zoo.php"><img src="Photos/zoo.jpg" alt="Jupiter MicroZoo" width="320" height="240"></a>
+        </div>
+        <div class="caption">
+            <h3>Jupiter MicroZoo</h3>
+            <a href="zoo.php" class="btn btn-primary" role="button">
+              Learn More
+            </a>             
+        </div>
+    </div>
+    </div>
+</div>
 
 <?php include 'footer.php'; ?>
--- Site Files/zoo.php
+++ Site Files/zoo.php
@@ -1,6 +1,11 @@
 <?php include 'header.php'; ?>
 
 <!-- Include HTML here -->
-<div style="text-align: center; padding-top: 10%; padding-bottom: 10%;"><span>MICROZOO Under Construction...</span></div>
+<div style="text-align: center; padding-top: 10%; padding-bottom: 10%;">
+    <p><font color="white"><strong>Micro Zoo</strong></font></p><br><br>
+    <span>
+    <font color="#337ab7">The Zoo:</font><br>Both a restaurant and a zoo, this gassy attraction will have you petting these adorable ugly things and then eating them at the famous all you can eat buffet! Only half of the creatures are poisoness, can you guess which ones? find out the hard way at the liability free petting area! (Please sign waiver before entering)<br><br>
+   
+</span></div>
 
 <?php include 'footer.php'; ?>
